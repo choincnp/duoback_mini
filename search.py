fada930de81de989e4e21870688229c2c1c2c49e
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-01-10 20:01:46 +0900

    docs: 기능명세서 추가

--- search.py
+++ search.py
@@ -8,7 +8,6 @@ REQ = {
     'VALUE': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
 }
 
-
 #Modules
 def getHTMLData(query):
     data = requests.get(f'https://www.youtube.com/results?search_query={query}', headers={REQ['KEY']: REQ['VALUE']})
