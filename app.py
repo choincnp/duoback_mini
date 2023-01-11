a9777d210e0b8021c49fbd0eae7bb6046890320f
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-01-11 16:27:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def signIn():
     return jsonify({'errorMessage': msg, 'error': error})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5001,debug=True)
+   app.run('0.0.0.0',port=5000,debug=True)
