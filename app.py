581a93139f7fa0f17ea477a3f8ce76e7025a90ab
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-01-11 15:22:12 +0900

    feat: Add selective redirect in Sign In

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def signIn():
         error = False
         db.users.insert_one({'id': inputId, 'pw': inputPw, 'playlist': []})
 
-    return jsonify({'errorMessage': msg, 'error': error})
+    return jsonify({'message': msg, 'error': error})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
